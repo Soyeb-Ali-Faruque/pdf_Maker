197506d72078d0eeab76a1a375d11271e8bd4099
Soyeb-Ali-Faruque <soyebali0101@gmail.com>
2024-01-11 18:47:55 +0530

    update profile page

--- pdf_Maker/pdf_Maker/views.py
+++ pdf_Maker/pdf_Maker/views.py
@@ -27,8 +27,8 @@ def home(request):
     
     user_id = request.session.get('user_id')
     user_information=None
-    data={'user':user_information,
-          'followMe':followMe}
+    data={'user':user_information
+    }
     
     try:
         user_information=userdata.objects.get(pk=user_id)
