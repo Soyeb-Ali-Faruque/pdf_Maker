1b8d129381bf8b29551806fcf167f76fcaf572bf
Soyeb-Ali-Faruque <soyebali0101@gmail.com>
2024-01-16 16:17:53 +0530

    solved admin panel error

--- pdf_Maker/userData/userInfo.py
+++ pdf_Maker/userData/userInfo.py
@@ -1,13 +1,18 @@
 from userData.models import userdata
 
 def userInformation(request):
-    user_id = request.session.get('user_id')
+    user_id = request.session.get('user_id',None)
     print(user_id)
     user_information = None
     
     try:
         if user_id is not None:
             user_information = userdata.objects.get(pk=user_id)
+        else:
+            # emergency fix default object
+            user_information = userdata.objects.get(pk=1)
+            
+            
     except userdata.DoesNotExist:
         print('User not found')
     except Exception as e:
