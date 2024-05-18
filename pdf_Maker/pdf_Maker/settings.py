2b63ffe3dae5b34aad5736d930f1bd2b286d61eb
Soyeb Ali Faruque <120319290+Soyeb-Ali-Faruque@users.noreply.github.com>
2024-05-18 17:54:01 +0530

    Update settings.py

--- pdf_Maker/pdf_Maker/settings.py
+++ pdf_Maker/pdf_Maker/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='s5tech.sendmail@gmail.com'
-EMAIL_HOST_PASSWORD='xfta ejlz ceox plaa'
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
 
 
